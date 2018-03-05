5e1c4eb4dd0492f83cf1971ac1c3d5d8643630ad
Yavor <javorkolev123@gmail.com>
2018-03-05 20:41:51 +0000

    Initial commit

---
+++ app/src/main/java/com/example/george/cookmeapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.george.cookmeapp;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    MaterialSearchView searchView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    homeFragment homeFragment = new homeFragment();
+                    FragmentManager homeManager = getFragmentManager();
+                    homeManager.beginTransaction().replace(R.id.containerLayout, homeFragment, homeFragment.getTag()).commit();
+                    return true;
+                case R.id.navigation_recipes:
+                    recipesFragment recipesFragment = new recipesFragment();
+                    FragmentManager recipesManager = getFragmentManager();
+                    recipesManager.beginTransaction().replace(R.id.containerLayout, recipesFragment, recipesFragment.getTag()).commit();
+                    return true;
+                case R.id.navigation_favourites:
+                    mTextMessage.setText(R.string.title_favourites);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.title_account);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("CookMeApp");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+    }
+}
